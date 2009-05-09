23125ffc0e7eac14e6c159d99a0e88b423296b3f
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-05-09 21:05:50 +0800

    ring benchmark exercise

---
+++ README.txt
@@ -0,0 +1,5 @@
+Write a ring benchmark. Create N processes in a ring. 
+
+Send a message around the ring M times so that a total of N * M messages get sent. 
+
+Time how long this takes for different values of N and M.
---
+++ ring.erl
@@ -0,0 +1,49 @@
+-module(ring).
+-compile(export_all).
+
+start(N, M) when is_integer(N), is_integer(M) ->
+    First = spawn(ring, first_loop, [nopid, M]),
+    io:format("First ~p~n", [First]),
+    Last = make_pid(N - 1, First),
+    io:format("***~n", []),
+    First ! {start, Last}.
+
+%first_loop(Pid, 0) ->
+%    io:format("Done sending messages~n", []),
+%    Pid ! done,
+%    done;
+first_loop(Pid, 0) ->
+    receive
+        ring ->
+            io:format("Done sending messages ~p~n", [self()]),
+            done
+    end;
+first_loop(Pid, M) ->
+    receive
+        ring ->
+            io:format("Starting next cycle ~p~n", [self()]),
+            Pid ! ring,
+            first_loop(Pid, M-1);
+        {start, LastPid} ->
+            LastPid ! ring,
+            first_loop(LastPid, M-1)
+    end.
+
+make_pid(0, Pid) ->
+    Pid;
+make_pid(N, Pid) ->
+    Next = spawn(ring, loop, [Pid]),
+    io:format("Next ~p~n", [Next]),
+    make_pid(N-1, Next).
+
+loop(Pid) ->
+    receive
+        done ->
+            io:format("done ~p~n", [self()]),
+            Pid ! done,
+            done;
+        ring ->
+            io:format("ring ~p~n", [self()]),
+            Pid ! ring,
+            loop(Pid)
+    end.
